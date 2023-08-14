1575b6b7801e954b980d2b577d75e86c910ad5e8
Aaditya Mohan <aadimohan51@gmail.com>
2023-08-14 21:37:03 +0530

    landing Page 80% Compelete.

---
+++ src/app/components/navbar/bnr-nav/bnr-nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bnr-nav',
+  templateUrl: './bnr-nav.component.html',
+  styleUrls: ['./bnr-nav.component.css']
+})
+export class BnrNavComponent {
+  h : string="Hello Nike App";
+  p : string="Download the app to access everything Nike.";
+  a : string = "Get your Great";
+}
